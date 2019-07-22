1deba2d067294221eef61337cba43fc6a2bd7246
goor <goor@innovid.com>
2019-07-22 15:13:40 +0300

    a b c d

--- Helloworld.rb
+++ Helloworld.rb
@@ -1,31 +1,28 @@
 class Helloworld < Formula
-    desc "an elf executable (golang) that prints hello world"
-    homepage "https://github.com/viggin543/opentikva"
-    version "0.3"
-    sha256 "1a4c78930ec0fd12ec475aae9424a3f5d2fe64096c8814f1a70a9f98252cee16"
-  
-    url "https://opentikva.s3.amazonaws.com/hello", :using => :curl
-  
-  
-    def install
-      system("ls","-la")
-      system("echo","installing hello world...")
-      system("pwd")
-  
-      puts `echo 'so it beggins...'`
-      
-      puts bin
-      
-      bin.install "hello"
-    end
-  
-    def uninstall
-      bin.uninstall "hello"
-    end
-  
-    def post_install
-      puts 'thank you for using this tap'
-    end
-  
-  
+  desc "an elf executable (golang) that prints hello world"
+  homepage "https://github.com/bravespacemonkey/brew_playground"
+  version "0.4"
+
+  url "https://opentikva.s3.amazonaws.com/hello", :using => :curl
+
+
+  def install
+    puts "Installing banana"
+    
+    puts bin
+    puts `ls -la`
+    puts `pwd`
+
+    bin.install "hello"
   end
+
+  def uninstall
+    bin.uninstall "hello"
+  end
+
+  def post_install
+    puts 'thank you for using this tap'
+  end
+
+
+end
