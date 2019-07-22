aa09e199530efe71da842e6bd648052869d85c81
goor <goor@innovid.com>
2019-07-22 15:10:02 +0300

    a b c

---
+++ hello.rb
@@ -0,0 +1,28 @@
+class Helloworld < Formula
+    desc "an elf executable (golang) that prints hello world"
+    homepage "https://github.com/bravespacemonkey/brew_playground"
+    version "0.1"
+  
+    url "https://opentikva.s3.amazonaws.com/hello", :using => :curl
+  
+  
+    def install
+      puts "Installing banana"
+      
+      puts bin
+      puts `ls -la`
+      puts `pwd`
+
+      bin.install "hello"
+    end
+  
+    def uninstall
+      bin.uninstall "hello"
+    end
+  
+    def post_install
+      puts 'thank you for using this tap'
+    end
+  
+  
+  end
